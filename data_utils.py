8fc79ecfa101121cfa128198e5524cf7d02ddf48
okocic <ognjen.kocic@rt-rk.com>
2018-07-26 21:18:30 +0200

    Refactor from playground to project

---
+++ data_utils.py
@@ -0,0 +1,92 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.utils import to_categorical
+import csv
+from skimage.filters import gaussian
+
+def load_dataset(n_data, fpath='emnist/emnist-balanced-train.csv', mpath='emnist/emnist-balanced-mapping.txt'):
+	X = np.zeros((n_data, 28, 28, 1), dtype=np.float32)
+	Y = np.zeros((n_data, 1), dtype=np.int32)
+	M = {}
+	with open(fpath) as csv_file:
+		reader = csv.reader(csv_file)
+		for idx, row in enumerate(reader):
+			Y[idx] = int(row[0])
+			X[idx] = np.transpose(np.reshape(np.array(row[1:], np.float32), (28, 28, 1)), [1, 0, 2])
+
+	with open(mpath) as map_file:
+		for line in map_file:
+			c_and_ascii = line.split()
+			M[int(c_and_ascii[0].strip())] = str(chr(int(c_and_ascii[1].strip())))
+	return X, Y, M
+
+
+def prepare_data(X, Y, M, subtract_mean_img=False):
+	n_class = len(list(M.keys()))
+	X = 2 * (X / 255.) - 1
+	Y = to_categorical(Y, n_class)
+	return X, Y, n_class
+
+
+def explore_dataset(X, Y, M, n_data, n_samples=5):
+	for idx in np.random.choice(n_data, n_samples):
+		print(M[Y[idx, 0]])
+		plt.imshow(X[idx, :, :, 0], cmap='gray')
+		plt.show()
+
+def plot_history(history):
+	# summarize history for accuracy
+	plt.plot(history.history['acc'])
+	plt.plot(history.history['val_acc'])
+	plt.title('model accuracy')
+	plt.ylabel('accuracy')
+	plt.xlabel('epoch')
+	plt.legend(['train', 'validation'], loc='upper left')
+	plt.show()
+
+	# summarize history for loss
+	plt.plot(history.history['loss'])
+	plt.plot(history.history['val_loss'])
+	plt.title('model loss')
+	plt.ylabel('loss')
+	plt.xlabel('epoch')
+	plt.legend(['train', 'validation'], loc='upper left')
+	plt.show()
+
+class ElasticDistortion:
+	def __init__(self, grid_shape, alpha=50, sigma=1.5):
+		self.x_grid_dim, self.y_grid_dim = grid_shape
+		self.x, self.y = np.meshgrid(np.arange(self.y_grid_dim), np.arange(self.x_grid_dim))
+		self.alpha = alpha
+		self.sigma = sigma
+
+	def _bilinear_interpolation(self, img, x, y):
+		x1 = np.floor(x).astype(np.int32)
+		x2 = x1 + 1
+		y1 = np.floor(y).astype(np.int32)
+		y2 = y1 + 1
+		x1 = np.clip(x1, a_min=0, a_max=img.shape[1] - 1)
+		x2 = np.clip(x2, a_min=0, a_max=img.shape[1] - 1)
+		y1 = np.clip(y1, a_min=0, a_max=img.shape[0] - 1)
+		y2 = np.clip(y2, a_min=0, a_max=img.shape[0] - 1)
+		r1 = (x2 - x) * img[y1, x1] + (x - x1) * img[y1, x2]
+		r2 = (x2 - x) * img[y2, x1] + (x - x1) * img[y2, x2]
+		return (y - y1) * r2 + (y2 - y) * r1
+
+	def __call__(self, in_img):
+		img = np.reshape(in_img, (in_img.shape[0], in_img.shape[1]))
+
+		dx = 2 * np.random.rand(*img.shape) - 1
+		dy = 2 * np.random.rand(*img.shape) - 1
+		dx = dx / np.linalg.norm(dx)
+		dy = dy / np.linalg.norm(dy)
+
+		fdx = gaussian(dx, sigma=self.sigma)
+		fdy = gaussian(dy, sigma=self.sigma)
+
+		sdx = self.alpha * fdx
+		sdy = self.alpha * fdy
+
+		indices = np.reshape(self.y + sdy, (-1, 1)), np.reshape(self.x + sdx, (-1, 1))
+		img = self._bilinear_interpolation(img, indices[1], indices[0])
+		return np.reshape(img, in_img.shape)
--- main.py
+++ main.py
@@ -1,39 +1,14 @@
 import matplotlib.pyplot as plt
-import csv
 import numpy as np
-from keras.utils import to_categorical
 from keras.layers import Conv2D, MaxPool2D, BatchNormalization, Dropout, Dense, Flatten, Input
 from keras.models import Model
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 from sklearn.model_selection import train_test_split
+from data_utils import *
 
 
 N_BALANCED_TRAIN = 112800
 INPUT_SHAPE = (28, 28, 1)
-
-def load_train(fpath='emnist/emnist-balanced-train.csv', mpath='emnist/emnist-balanced-mapping.txt'):
-	X = np.zeros((N_BALANCED_TRAIN, 28, 28, 1), dtype=np.float32)
-	Y = np.zeros((N_BALANCED_TRAIN, 1), dtype=np.int32)
-	M = {}
-	with open(fpath) as csv_file:
-		reader = csv.reader(csv_file)
-		for idx, row in enumerate(reader):
-			Y[idx] = int(row[0])
-			X[idx] = np.transpose(np.reshape(np.array(row[1:], np.float32), (28, 28, 1)), [1, 0, 2])
-
-	with open(mpath) as map_file:
-		for line in map_file:
-			c_and_ascii = line.split()
-			M[int(c_and_ascii[0].strip())] = str(chr(int(c_and_ascii[1].strip())))
-	return X, Y, M
-
-
-def prepare_data(X, Y, M, subtract_mean_img=False):
-	n_class = len(list(M.keys()))
-	X = 2 * (X / 255.) - 1
-	Y = to_categorical(Y, n_class)
-	return X, Y, n_class
-
 
 def get_model(n_class, print_summary=False):
 	x = Input(shape=INPUT_SHAPE)
@@ -68,32 +43,8 @@ def get_model(n_class, print_summary=False):
 		model.summary()
 	return model
 
-def explore_dataset(X, Y, M, n_samples=5):
-	for idx in np.random.choice(N_BALANCED_TRAIN, n_samples):
-		print(M[Y[idx, 0]])
-		plt.imshow(X[idx, :, :, 0], cmap='gray')
-		plt.show()
 
-def plot_history(history):
-	# summarize history for accuracy
-	plt.plot(history.history['acc'])
-	plt.plot(history.history['val_acc'])
-	plt.title('model accuracy')
-	plt.ylabel('accuracy')
-	plt.xlabel('epoch')
-	plt.legend(['train', 'test'], loc='upper left')
-	plt.show()
-
-	# summarize history for loss
-	plt.plot(history.history['loss'])
-	plt.plot(history.history['val_loss'])
-	plt.title('model loss')
-	plt.ylabel('loss')
-	plt.xlabel('epoch')
-	plt.legend(['train', 'test'], loc='upper left')
-	plt.show()
-
-raw_train_x, raw_train_y, class_map = load_train()
+raw_train_x, raw_train_y, class_map = load_dataset(N_BALANCED_TRAIN)
 # explore_dataset(train_x, train_y, train_map)
 train_x_all, train_y_all, n_class = prepare_data(raw_train_x, raw_train_y, class_map)
 train_x, validation_x, train_y, validation_y = train_test_split(train_x_all, train_y_all, test_size=0.2, random_state=42)
